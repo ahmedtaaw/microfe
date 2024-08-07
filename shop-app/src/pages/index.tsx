03b07b5cef76855c2a90897ecbdae65f11080912
ahmed.tawfik <ahmed.tawfik@saab.ae>
2024-08-07 12:20:04 +0400

    Create Navbar and Footer components in main-app, import them into index.js; create Products component in shop-app, and import it into index.js.

--- shop-app/src/pages/index.tsx
+++ shop-app/src/pages/index.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Products from '@/components/Products';
 
 export default function Home() {
   return (
-    <main className='h-[100vh] flex justify-center items-center'>
-      Hello shop app
-    </main>
+      <Products />
   );
 }
