02d4c7a8b61e0b8c3bf258dae1d355cb3792a367
ahmed.tawfik <ahmed.tawfik@saab.ae>
2024-08-07 12:13:30 +0400

    mainapp/navbar and footer

--- main-app/src/pages/index.tsx
+++ main-app/src/pages/index.tsx
@@ -1,12 +1,16 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <main className='h-[100vh] flex justify-center items-center'>
-      Hello main app
+    <main className='flex flex-col bg-white'>
+      <Navbar />
+      <div className='h-[75vh] py-4'>
+        <h1 className='text-3xl font-bold font-mono h-[100%] flex justify-center items-center text-black'>
+          Welcome to Main Application
+        </h1>
+      </div>
+      <Footer />
     </main>
   );
 }
