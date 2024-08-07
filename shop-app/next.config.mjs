721e8b571f8a392dea1dbccc35b7ebd38e58d2b9
ahmed.tawfik <ahmed.tawfik@saab.ae>
2024-08-07 12:10:11 +0400

    Set up app name, build file, expose/remote/shared objects, assign port in package.json, and update index.js files for main-app and shop-app

--- shop-app/next.config.mjs
+++ shop-app/next.config.mjs
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
 const nextConfig = {
-  reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'shop_app',
+        remotes: {},
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {},
+        extraOptions: {
+          debug: false, // `false` by default
+          exposePages: false, // `false` by default
+        },
+        shared: {},
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
